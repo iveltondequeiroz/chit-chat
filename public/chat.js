c20356440c50e46c8097b656fe2c946cda00a6a9
@ivelton2018 <ivelton2018@gmail.com>
2018-11-06 10:19:33 -0400

    broadcasting messages

--- public/chat.js
+++ public/chat.js
@@ -10,16 +10,24 @@ var message = document.getElementById('message'),
       feedback = document.getElementById('feedback')
 
 // emit events
-btn.addEventListener('click', function(){
+btn.addEventListener('click', () => {
     socket.emit('chat', {
         message: message.value,
         handle: handle.value
     });
-    message.value = "";
+    message.value = ""
+})
+
+message.addEventListener('keypress', () => {
+    socket.emit('typing', handle.value)
 })
 
 // listen for events
 socket.on('chat', data => {
+	feedback.innerHTML = ''
 	output.innerHTML += '<p><strong>'+data.handle+':'+'</strong>'+data.message+'</p>'
 })
-      
+
+socket.on('typing', (data) => {
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
